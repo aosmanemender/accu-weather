5dff59ce83bfd5d22a3fc7fc83eeeff7c6ba789f
osman <osman@payleadr.com>
2023-09-19 00:47:51 +0300

    exclude xtension

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const os = require("os")
-const client_id = os.environ["client_id"]
-// const client_id = 123
+// const client_id = os.environ["client_id"]
+const client_id = 123
 console.log(`client_id: ${client_id}`)
